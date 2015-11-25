5317942ea60c537b7cb5c95395c0c0ce36447e3f
Jorge Tardaguila <jtardaguila@users.noreply.github.com>
2015-11-25 17:25:16 -0300

    Update CSVRecordTest.java

--- src/test/java/org/apache/commons/csv/CSVRecordTest.java
+++ src/test/java/org/apache/commons/csv/CSVRecordTest.java
@@ -16,14 +16,7 @@
  */
 package org.apache.commons.csv;
 
-import java.util.List;
 
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.util.ArrayList;
